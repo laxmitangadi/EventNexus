954533bdf01084fb6c0865da58f42d7329e2494a
laxmitangadi <lakshlaxmi2001@gmail.com>
2025-04-06 15:54:15 +0530

    This is Register and Login Page

---
+++ frontend/src/components/LoginPage.jsx
@@ -0,0 +1,74 @@
+import React, { useRef, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/AuthPages.css';
+
+const LoginPage = () => {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    let particles = [];
+
+    class Particle {
+      constructor() {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.size = Math.random() * 2;
+        this.speedX = Math.random() * 3 - 1.5;
+        this.speedY = Math.random() * 3 - 1.5;
+      }
+
+      update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+        if (this.x > canvas.width) this.x = 0;
+        if (this.x < 0) this.x = canvas.width;
+        if (this.y > canvas.height) this.y = 0;
+        if (this.y < 0) this.y = canvas.height;
+      }
+
+      draw() {
+        ctx.fillStyle = '#9D00FF';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fill();
+      }
+    }
+
+    const init = () => {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      particles = Array(100).fill().map(() => new Particle());
+    };
+
+    const animate = () => {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      particles.forEach(p => {
+        p.update();
+        p.draw();
+      });
+      requestAnimationFrame(animate);
+    };
+
+    init();
+    animate();
+    window.addEventListener('resize', init);
+    return () => window.removeEventListener('resize', init);
+  }, []);
+
+  return (
+    <div className="auth-page">
+      <canvas ref={canvasRef} className="background-canvas" />
+      <form className="auth-form">
+        <h2>Login</h2>
+        <input type="text" placeholder="Username" required />
+        <input type="password" placeholder="Password" required />
+        <button type="submit">Login</button>
+        <p>Don't have an account? <Link to="/register">Register here</Link></p>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
